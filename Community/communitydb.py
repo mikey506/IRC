654898e99a4a53d099f7ad8ae09d11880cc14380
Michael Landry <michaellandry3@hotmail.com>
2023-05-20 08:01:08 -0300

    Update communitydb.py
    Added !setcommunity function to allow changes within the json file for specific communities and specific variables. 
    
    Syntax: !setcommunity <city/town from geo.db> <community name> <variable> <value>
    
    Variables: community_name , total_members , platform , url , mods

--- Community/communitydb.py
+++ Community/communitydb.py
@@ -1,12 +1,11 @@
 import irc.bot
 import json
 import time
-from math import radians, cos, sin, sqrt, asin
 
 # ANSI color escape sequences
 BLUE = "\x02\x0302"
 RED = "\x02\x0304"
-RESET = "\x0f"
+RESET = "\x0F"
 
 class MyBot(irc.bot.SingleServerIRCBot):
     def __init__(self, server, port, nickname, channel):
@@ -46,6 +45,16 @@ class MyBot(irc.bot.SingleServerIRCBot):
                 city_tag = args[0]
                 community_name = args[1]
                 self.remove_community(connection, event.target, city_tag, community_name)
+        elif message.startswith('!setcommunity'):
+            args = message.split()[1:]
+            if len(args) < 4:
+                self.send_message(connection, event.target, f"{RED}Usage: !setcommunity <city/town from geo.db> <community name> <variable> <value>{RESET}")
+            else:
+                city_tag = args[0]
+                community_name = args[1]
+                variable = args[2]
+                value = ' '.join(args[3:])
+                self.set_community(connection, event.target, city_tag, community_name, variable, value)
 
     def send_message(self, connection, target, message):
         message = message.replace('\r', '').replace('\n', '')  # Remove carriage returns and newlines
@@ -68,14 +77,15 @@ class MyBot(irc.bot.SingleServerIRCBot):
             with open(filename, 'r') as file:
                 communities = json.load(file)
             if communities:
-                self.send_message(connection, target, f"Community in {BLUE}{city}{RESET}:")
+                self.send_message(connection, target, f"Communities in {BLUE}{city}{RESET}:")
                 for community in communities:
-                    community_name = community['community_name']
+                    community_name = community['community_name'].replace('_', ' ')
                     total_members = community['total_members']
                     platform = community['platform']
                     url = community['url']
                     mods = ', '.join(community['mods'])
-                    self.send_message(connection, target, f"Name: {BLUE}{community_name}{RESET} | Total Members/Subscribers: {BLUE}{total_members}{RESET} | Platform: {BLUE}{platform}{RESET} | URL: {BLUE}{url}{RESET} | Mods: {BLUE}{mods}{RESET}")
+                    message = f"Name: {BLUE}{community_name}{RESET} | Total Members/Subscribers: {BLUE}{total_members}{RESET} | Platform: {BLUE}{platform}{RESET} | URL: {BLUE}{url}{RESET} | Mods: {BLUE}{mods}{RESET}"
+                    self.send_message(connection, target, message)
             else:
                 self.send_message(connection, target, f"No communities found for {city}")
         except FileNotFoundError:
@@ -136,11 +146,38 @@ class MyBot(irc.bot.SingleServerIRCBot):
         except json.JSONDecodeError:
             self.send_message(connection, target, f"Invalid JSON file for {city_tag}")
 
-if __name__ == '__main__':
-    server = 'irc.server.com'  # IRC server hostname
-    port = 6667  # IRC server port
-    nickname = 'MyBot'  # Bot nickname
-    channel = '#mychannel'  # Channel to join
+    def set_community(self, connection, target, city_tag, community_name, variable, value):
+        # Set a specific variable of a community in the json file
+        filename = f'./communities/{city_tag}.json'
+        try:
+            with open(filename, 'r+') as file:
+                communities = json.load(file)
+                modified = False
+                for community in communities:
+                    if community['community_name'] == community_name:
+                        if variable in community:
+                            community[variable] = value
+                            modified = True
+                            break
+                if modified:
+                    file.seek(0)
+                    json.dump(communities, file, indent=4)
+                    file.truncate()
+                    self.send_message(connection, target, f"Variable '{variable}' of community '{community_name}' in {city_tag} set to '{value}'")
+                else:
+                    self.send_message(connection, target, f"Community '{community_name}' not found in {city_tag}")
+        except FileNotFoundError:
+            self.send_message(connection, target, f"No communities found for {city_tag}")
+        except json.JSONDecodeError:
+            self.send_message(connection, target, f"Invalid JSON file for {city_tag}")
 
+def main():
+    server = 'irc.server.com'
+    port = 6667
+    nickname = 'MyBot'
+    channel = '#mychannel'
     bot = MyBot(server, port, nickname, channel)
     bot.start()
+
+if __name__ == '__main__':
+    main()
